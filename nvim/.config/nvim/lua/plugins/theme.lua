8ffcb33ec506a19f98ae269d8977be22ab63a82b
anasahmd <anasahmad0239@gmail.com>
2026-01-10 20:04:19 +0530

    Add rofi config

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -7,7 +7,17 @@ return {
     opts = {
       flavour = "macchiato",
       transparent_background = true,
-      auto_integration = true,
+      default_integrations = true,
+      auto_integrations = true,
+
+      custom_highlights = function(colors)
+        return {
+          NeoTreeNormal = { bg = "NONE" },
+          NeoTreeNormalNC = { bg = "NONE" },
+          NeoTreeEndOfBuffer = { bg = "NONE" }, -- Fixes the empty space at the bottom
+          NeoTreeWinSeparator = { bg = "NONE", fg = colors.surface1 },
+        }
+      end,
     },
     config = function(_, opts)
       require("catppuccin").setup(opts)
